d0deedc3435314f17583a76b1b3bacab734e2ff5
modernxpunk <modernxpunk@gmail.com>
2023-05-12 15:14:26 +0300

    seed db

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,21 +1,30 @@
 import { PrismaClient } from "@prisma/client";
 import { faker } from "@faker-js/faker";
-
-const generateUser = (usersLength: number) => {
-	let users = [];
-	for (let i = 0; i < usersLength; i++) {
-		users.push({
-			name: faker.name.fullName(),
-		});
-	}
-	return users;
-};
+import { getRandomImgSrc } from "../src/utils/view";
 
 const prisma = new PrismaClient();
 
 async function main() {
-	await prisma.person.createMany({
-		data: generateUser(100),
+	const catalogs = faker.helpers
+		.uniqueArray(faker.commerce.product, 10)
+		.map((name) => ({ name }));
+
+	const products = Array(100)
+		.fill(0x00)
+		.map(() => ({
+			name: faker.commerce.productName(),
+			price: +faker.commerce.price(100, 1000, 0),
+			catalogId: +faker.datatype.number({ min: 1, max: catalogs.length }),
+			image: getRandomImgSrc(),
+			description: faker.commerce.productDescription(),
+		}));
+
+	await prisma.catalog.createMany({
+		data: catalogs,
+		skipDuplicates: true,
+	});
+	await prisma.product.createMany({
+		data: products,
 		skipDuplicates: true,
 	});
 }
