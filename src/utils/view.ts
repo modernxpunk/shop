c3adc323983050e7868e2408e7e30697d50ac159
modernxpunk <modernxpunk@gmail.com>
2023-05-12 15:13:20 +0300

    update get random img src

--- src/utils/view.ts
+++ src/utils/view.ts
@@ -2,9 +2,17 @@ const getRandomNumber = (min: number, max: number): number => {
 	return Math.ceil((min + Math.floor(Math.random() * (max - min))) / 100) * 100;
 };
 
-export const getRandomImgSrc = () => {
-	return `https://fakeimg.pl/${getRandomNumber(100, 1000)}x${getRandomNumber(
-		100,
-		1000
-	)}/`;
+export const getRandomImgSrc = (
+	x: number | null = null,
+	y: number | null = null
+) => {
+	let width = x;
+	let height = y;
+	if (!width) {
+		width = getRandomNumber(100, 1000);
+	}
+	if (!height) {
+		height = getRandomNumber(100, 1000);
+	}
+	return `https://fakeimg.pl/${width}x${height}/`;
 };
