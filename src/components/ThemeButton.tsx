27595031063df89c53701b1caa833203dc3f42cc
modernxpunk <modernxpunk@gmail.com>
2023-05-14 12:42:58 +0300

    change theme button

--- src/components/ThemeButton.tsx
+++ src/components/ThemeButton.tsx
@@ -1,44 +1,19 @@
 "use client";
-import { cx } from "class-variance-authority";
-import { useEffect, useState } from "react";
-import { themeChange } from "theme-change";
 import Icon from "./Icon";
 
 const ThemeButton = () => {
-	const [theme, setTheme] = useState<"light" | "dark">("light");
-
-	useEffect(() => {
-		themeChange(false);
-	}, []);
-
 	return (
 		<>
-			<label className="gap-1 swap">
-				<input
-					type="checkbox"
-					checked={theme === "light"}
-					onChange={(e) => setTheme(e.target.checked ? "dark" : "light")}
+			<label className="swap">
+				<input type="checkbox" />
+				<Icon
+					name="white-balance-sunny"
+					className="w-16 h-8 fill-current swap btn btn-sm btn-ghost swap-on"
 				/>
-				<button
-					className="swap-on"
-					data-toggle-theme="light"
-					data-set-theme="light"
-				>
-					<Icon
-						name="white-balance-sunny"
-						className={cx("w-16 h-8 fill-current swap btn btn-sm btn-ghost")}
-					/>
-				</button>
-				<button
-					className="swap-off"
-					data-toggle-theme="dark"
-					data-set-theme="dark"
-				>
-					<Icon
-						name="moon-waning-crescent"
-						className={cx("w-16 h-8 fill-current swap btn btn-sm btn-ghost")}
-					/>
-				</button>
+				<Icon
+					name="moon-waning-crescent"
+					className="w-16 h-8 fill-current swap btn btn-sm btn-ghost swap-off"
+				/>
 			</label>
 		</>
 	);
