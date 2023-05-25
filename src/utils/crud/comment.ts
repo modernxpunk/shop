0ec0ca0208303c90ebd61620c674e828a9103135
modernxpunk <modernxpunk@gmail.com>
2023-05-25 14:28:02 +0300

    rid of use server directive

---
+++ src/utils/crud/comment.ts
@@ -0,0 +1,9 @@
+// "use server";
+
+import { prisma } from "../db";
+
+const createComment = async (text: string) => {};
+
+const deleteComment = async (id: string) => {};
+
+export { createComment, deleteComment };
