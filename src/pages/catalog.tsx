dc9f9759f58aa9deb9a26a62813616d88cbc7cb0
modernxpunk <modernxpunk@gmail.com>
2023-06-06 13:32:47 +0300

    delet catalogFilter, update /catalog page

--- src/pages/catalog.tsx
+++ src/pages/catalog.tsx
@@ -1,16 +1,21 @@
+import { Catalog } from "@prisma/client";
 import { createServerSideHelpers } from "@trpc/react-query/server";
 import Card from "src/components/Card";
-import CatalogFilter from "src/components/CatalogFilter";
 import Icon from "src/components/Icon";
+import { createContext } from "src/server/context";
 import appRouter from "src/server/routes/_app";
 import { trpc } from "src/utils/trpc";
+import superjson from "superjson";
+import { useState } from "react";
+import CardSkeleton from "src/components/CardSkeleton";
 
 export const getServerSideProps = async (req: any) => {
 	const catalog = req.query.catalog || "";
 
 	const ssr = createServerSideHelpers({
 		router: appRouter,
-		ctx: {},
+		ctx: await createContext(req),
+		transformer: superjson,
 	});
 
 	await ssr.catalog.getAll.prefetch();
@@ -24,20 +29,116 @@ export const getServerSideProps = async (req: any) => {
 	};
 };
 
-const Catalogs = ({ catalog }: any) => {
-	const { data: catalogs }: any = trpc.catalog.getAll.useQuery();
-	const { data: products }: any =
-		trpc.catalog.getProductByCatalogName.useQuery(catalog);
+const Catalogs = ({ catalog }: { catalog: string }) => {
+	const [selectedCatalog, setSelectedCatalog] = useState(catalog);
+	const [selectedFilter, setSelectedFilter] = useState("sort by");
+
+	const { data: catalogs } = trpc.catalog.getAll.useQuery();
+	const { data: products, isLoading: isLoadingProducts } =
+		trpc.catalog.getProductByCatalogName.useQuery(selectedCatalog);
+
 	return (
 		<>
 			<div className="container flex h-full gap-4">
-				<CatalogFilter catalogs={catalogs} currentCatalog={catalog} />
+				<div className="hidden md:block flex-1 max-w-[250px]">
+					<div className="sticky hidden bg-base-200 sm:block rounded-box top-20">
+						<div className="collapse collapse-arrow">
+							<input
+								type="checkbox"
+								className="peer"
+								defaultChecked={!!catalog}
+							/>
+							<div className="collapse-title">Categories</div>
+							<div className="collapse-content">
+								{catalogs &&
+									catalogs.map((catalog: Catalog) => {
+										return (
+											<label
+												className="flex items-center gap-2 py-1"
+												id="category"
+												key={catalog.id + catalog.name}
+												onClick={() => setSelectedCatalog(catalog.name)}
+											>
+												<input
+													type="radio"
+													name="category"
+													className="radio radio-sm"
+													readOnly
+													checked={selectedCatalog === catalog.name}
+												/>
+												<p>{catalog.name}</p>
+											</label>
+										);
+									})}
+							</div>
+						</div>
+						<div className="collapse collapse-arrow">
+							<input type="checkbox" className="peer" />
+							<div className="collapse-title">Price</div>
+							<div className="collapse-content">
+								<input
+									type="range"
+									min="0"
+									max="100"
+									className="range"
+									readOnly
+									step="25"
+								/>
+								<div className="flex justify-between w-full px-2 text-xs">
+									<span>|</span>
+									<span>|</span>
+									<span>|</span>
+									<span>|</span>
+									<span>|</span>
+								</div>
+							</div>
+						</div>
+						<div className="collapse collapse-arrow">
+							<input type="checkbox" className="peer" />
+							<div className="collapse-title">Rank</div>
+							<div className="collapse-content">
+								<div className="flex flex-col gap-4">
+									{Array(5)
+										.fill(0)
+										.map((_, i) => {
+											return (
+												<div key={i} className="flex items-center gap-2">
+													<input
+														type="checkbox"
+														className="checkbox checkbox-sm"
+													/>
+													<div className="rating rating-md">
+														{Array(5)
+															.fill(0)
+															.map((_, j) => {
+																return (
+																	<input
+																		type="radio"
+																		name={`rating-${20 * i + 1 + j}as`}
+																		readOnly
+																		className="mask mask-star"
+																		checked={4 - i <= j}
+																		key={j}
+																	/>
+																);
+															})}
+													</div>
+												</div>
+											);
+										})}
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
 				<div className="flex-1 rounded-box">
-					<div className="sticky z-20 top-16">
+					<div className="sticky z-10 top-16">
 						<div className="flex items-center justify-between backdrop-blur-lg">
 							<h1 className="text-[2.5rem] font-bold">
 								Catalog
-								<span className="text-sm opacity-60">({products.length})</span>
+								<span className="text-sm opacity-60">
+									({products && products.length})
+								</span>
 							</h1>
 							<div className="flex items-center justify-end gap-2">
 								<div className="hidden btn-group md:inline-flex">
@@ -50,7 +151,8 @@ const Catalogs = ({ catalog }: any) => {
 								</div>
 								<select
 									className="hidden max-w-xs select select-sm select-bordered md:inline-flex"
-									defaultValue={"sort by"}
+									value={selectedFilter}
+									onChange={(e) => setSelectedFilter(e.target.value)}
 								>
 									<option value={"sort by"} disabled>
 										Sort by
@@ -63,9 +165,17 @@ const Catalogs = ({ catalog }: any) => {
 					</div>
 					<div>
 						<div className="grid gap-4 grid-cols-item">
-							{products.map((product: any) => (
-								<Card product={product} key={product.id} />
-							))}
+							{!isLoadingProducts &&
+								products &&
+								products.map((product: any) => (
+									<Card product={product} key={product.id} />
+								))}
+							{isLoadingProducts &&
+								Array(12)
+									.fill(0x00)
+									.map((_, i) => (
+										<CardSkeleton key={`product_skeleton_${i}`} />
+									))}
 						</div>
 					</div>
 				</div>
