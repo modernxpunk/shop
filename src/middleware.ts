03b9118f967586225a6d20afe8def6d7af6bb5d7
OleksandrKovalskyi <modernxpunk@gmail.com>
2025-08-11 10:58:36 +0300

    update

--- src/middleware.ts
+++ src/middleware.ts
@@ -47,7 +47,7 @@ export function middleware(req: NextRequest) {
 	const country = req.headers.get("cf-ipcountry") || "";
 	const region = req.headers.get("cf-region-code") || "";
 
-	console.log(`Country: ${country}, Region: ${region}`, [...req.headers.entries()]);
+	console.log(`Country: ${country}, Region: ${region}`, req);
 
 	if (BLOCKED_COUNTRIES.includes(country) || BLOCKED_REGIONS.includes(region)) {
 		const pathname = new URL(req.url).pathname;
